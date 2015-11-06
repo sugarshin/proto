2821aea9798aeed50ef8b7ce0a44b5d1dd06bb88
sato <sato@tsumikiinc.com>
2015-11-06 17:34:59 +0900

    edit

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 npm i -D babel-preset-es2015 babel-preset-react babel-preset-stage-0 babelify budo garnish
 npm i -S babel-polyfill
-touch index.js
+echo "import 'babel-polyfill';" >> index.js
 rm -rf .git init.sh
