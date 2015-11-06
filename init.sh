61137ca4a1e0051aab856d9edeaab83be2ae71b7
sato <sato@tsumikiinc.com>
2015-11-06 13:27:38 +0900

    bump babel to 6

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
-npm i -D babelify budo garnish
-touch main.js
+npm i -D babel-preset-es2015 babel-preset-react babel-preset-stage-0 babelify budo garnish
+npm i -S babel-polyfill
+touch index.js
 rm -rf .git init.sh
